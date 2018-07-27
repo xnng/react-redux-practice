e55064c1b8c06793900ab93834b23edfa37125e6
xnng <xnng77@gmail.com>
2018-07-27 14:21:48 +0800

    jquery ajax

--- react-accounts/src/components/Record.js
+++ react-accounts/src/components/Record.js
@@ -5,9 +5,9 @@ class Record extends Component {
     render() {
         return (
             <tr>
-                <td>{this.props.record.date}</td>
-                <td>{this.props.record.title}</td>
-                <td>{this.props.record.amount}</td>
+                <td>{this.props.date}</td>
+                <td>{this.props.title}</td>
+                <td>{this.props.amount}</td>
             </tr>
         );
     }
--- react-accounts/src/components/Records.js
+++ react-accounts/src/components/Records.js
@@ -1,35 +1,55 @@
 import React, { Component } from 'react';
 import Record from './Record';
+import { getJSON } from 'jquery';
 
 class Records extends Component {
   constructor() {
     super()
     this.state = {
-      records: [
-        { "id": 2, "date": "2018-08-01", "title": "test1", "amount": 10 },
-        { "id": 3, "date": "2018-08-02", "title": "test2", "amount": 20 },
-        { "id": 4, "date": "2018-08-03", "title": "test3", "amount": 30 }
-      ]
+      error: null,
+      isLoaded: false,
+      records: []
     }
   }
 
+  componentDidMount() {
+    getJSON("https://5b5a93b350bab80014e5f79c.mockapi.io/api/v1/records")
+      .then(
+        response => this.setState({
+          records: response,
+          isLoaded: true
+        }),
+        error => this.setState({
+          isLoaded: true,
+          error
+        })
+      )
+  }
+
   render() {
-    return (
-      <div className="container">
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Title</th>
-              <th>Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.records.map((record) => <Record record={record} key={record.id}/>)}
-          </tbody>
-        </table>
-      </div>
-    );
+    const { error, isLoaded, records} = this.state;
+    if (error) {
+      return <div>Error: {error.responseText}</div>
+    } else if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+        <div className="container">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Title</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {records.map((record) => <Record {...record} key={record.id} />)}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
   }
 }
 
