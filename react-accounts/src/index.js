1ae65c5e329da227c52e3dc7149f26c3730c4f72
xnng <xnng77@gmail.com>
2018-08-01 13:38:18 +0800

    restart

--- react-accounts/src/index.js
+++ react-accounts/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Records from './components/Records';
+// import Records from './components/Records';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Records />, document.getElementById('root'));
