9d735d36d39c450cdbbd55942120bb526d633dc2
xnng <xnng77@gmail.com>
2018-08-03 20:40:08 +0800

    添加表格的两种状态转换

--- react-accounts/src/components/RecordTable.js
+++ react-accounts/src/components/RecordTable.js
@@ -2,7 +2,23 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class RecordTable extends Component {
-    render() {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            edit: true
+        }
+    }
+
+    handleToggle = () => {
+        this.setState({
+            edit: !this.state.edit
+        })
+    }
+
+
+    recordRow() {
         return (
             <tr>
                 <td>{this.props.date}</td>
@@ -10,12 +26,48 @@ export default class RecordTable extends Component {
                 <td>{this.props.amount}</td>
                 <td>
                     <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-info"><i className="fa fa-pencil" /></button>
+                        <button type="button" className="btn btn-sm btn-info" onClick={this.handleToggle}><i className="fa fa-pencil" /></button>
                         <button type="button" className="btn btn-sm btn-danger"><i className="fa fa-trash" /></button>
                     </div>
                 </td>
             </tr>
         )
+    }
+
+    recordNewRow() {
+        return (
+            <tr>
+                <td>
+                    <div className="input-group input-group-sm">
+                        <input type="text" className="form-control input-group-prepend" defaultValue={this.props.date} />
+                    </div>
+                </td>
+                <td>
+                    <div className="input-group input-group-sm">
+                        <input type="text" className="form-control input-group-prepend" defaultValue={this.props.title} />
+                    </div>
+                </td>
+                <td>
+                    <div className="input-group input-group-sm">
+                        <input type="text" className="form-control input-group-prepend" defaultValue={this.props.amount} />
+                    </div>
+                </td>
+                <td>
+                    <div className="btn-group">
+                        <button type="button" className="btn btn-sm btn-info"><i className="fa fa-check" /></button>
+                        <button type="button" className="btn btn-sm btn-danger" onClick={this.handleToggle}><i className="fa fa-close" /></button>
+                    </div>
+                </td>
+            </tr>
+        )
+    }
+
+    render() {
+        if (this.state.edit) {
+            return this.recordRow();
+        } else {
+            return this.recordNewRow();
+        }
     }
 }
 
