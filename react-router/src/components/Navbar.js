d3c938734ab0917697000c529515b004c09574f4
xnng <xnng77@gmail.com>
2018-08-07 14:34:12 +0800

    add vue-todo

--- react-router/src/components/Navbar.js
+++ react-router/src/components/Navbar.js
@@ -8,7 +8,7 @@ export default class Navbar extends Component {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="navbar-brand">
                     <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg"
-                        width="30" height="30" className="d-inline-block align-top" />&nbsp;
+                        width="30" height="30" className="d-inline-block align-top" alt="logo"/>&nbsp;
                 Bootstrap
                 </div>
                 <div className="collapse navbar-collapse">
