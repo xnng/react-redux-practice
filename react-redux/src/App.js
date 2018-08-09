b599cb69f9987d7741f859617d39d0cf790128ed
xnng <xnng77@gmail.com>
2018-08-09 16:19:18 +0800

    优化 redux

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import reducer from './reducer/Counter'
+import { createStore } from 'redux'
+import { increment, decrement } from './actions';
+
+const store = createStore(reducer)
+const initCount = store.getState()
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: initCount
+    }
+  }
+
+  onIncrement = () => {
+    store.dispatch(increment())
+    this.setState({
+      count: store.getState()
+    })
+  }
+
+  onDecrement = () => {
+    store.dispatch(decrement())
+    this.setState({
+      count: store.getState()
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <h1 className="text-center mt-5">{this.props.value}</h1>
+        <h1 className="text-center mt-5">{this.state.count}</h1>
         <p className="text-center">
-          <button onClick={this.props.onIncrement} className="btn btn-primary mr-2">Increase</button>
-          <button onClick={this.props.onDecrement} className="btn btn-danger my-2">Decrease</button>
+          <button onClick={this.onIncrement} className="btn btn-primary mr-2">Increase</button>
+          <button onClick={this.onDecrement} className="btn btn-danger my-2">Decrease</button>
         </p>
       </div>
     );
   }
-}
-
-App.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired,
 }
 
 export default App;
--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reducer from './reducer/Counter'
-import { createStore } from 'redux'
-import { increment, decrement } from './actions';
 
-const store = createStore(reducer)
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const render = () => {
-    ReactDOM.render(
-        <App
-            onIncrement={() => store.dispatch(increment())}
-            onDecrement={() => store.dispatch(decrement())}
-            value={store.getState()} />,
-        document.getElementById('root'));
-}
-
-render()
-store.subscribe(render)
