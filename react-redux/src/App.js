aead7148d2ef149a28039a474fa5b3cb2e9cd565
xnng <xnng77@gmail.com>
2018-08-09 16:56:54 +0800

    脱离 setState 的 redux

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,39 +1,24 @@
 import React, { Component } from 'react';
-import reducer from './reducer/Counter'
-import { createStore } from 'redux'
-import { increment, decrement } from './actions';
+import { createStore } from 'redux';
+import { decrement, increment } from './actions';
+import reducer from './reducer/Counter';
 
-const store = createStore(reducer)
-const initCount = store.getState()
+export const store = createStore(reducer)
 
 class App extends Component {
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      count: initCount
-    }
-  }
-
   onIncrement = () => {
     store.dispatch(increment())
-    this.setState({
-      count: store.getState()
-    })
   }
 
   onDecrement = () => {
     store.dispatch(decrement())
-    this.setState({
-      count: store.getState()
-    })
   }
 
   render() {
     return (
       <div className="container">
-        <h1 className="text-center mt-5">{this.state.count}</h1>
+        <h1 className="text-center mt-5">{store.getState()}</h1>
         <p className="text-center">
           <button onClick={this.onIncrement} className="btn btn-primary mr-2">Increase</button>
           <button onClick={this.onDecrement} className="btn btn-danger my-2">Decrease</button>
--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { store } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+const render = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+render();
+store.subscribe(render);
