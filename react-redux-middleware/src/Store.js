e2e4e3b162a6bd0571a50c22fa825852132a659f
xnng <xnng77@gmail.com>
2018-08-21 16:35:08 +0800

    redux-logger

--- react-redux-middleware/src/Store.js
+++ react-redux-middleware/src/Store.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
-import rootReducer from './reducer/CombineReducer';
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "./reducer/CombineReducer";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
