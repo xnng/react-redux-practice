1d3e6cd7c4b3cbf0573473347a136f98e23cd24a
xnng <xnng77@gmail.com>
2018-08-26 21:58:47 +0800

    reminder init

--- react-redux-crud/reminder/src/index.js
+++ react-redux-crud/reminder/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker(); 
